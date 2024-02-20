b7c89bdbf13573445280a89760da1cfe9d76d6de
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2024-02-20 19:06:22 +0100

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,47 @@
+<template>
+    <main class="container">
+        <div class="row">
+            <div class="col-12">
+                <ul>
+                    <li v-for="project in projects" :key="project.id">
+                        {{ project.title }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </main>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            projects: []
+        }
+    },
+    methods: {
+        getProjects(){
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                }
+            })
+            .then((response) => {
+                console.log(response.data.results);
+                this.projects = response.data.results;
+
+            })
+            .catch(function (error) {
+                console.warn(error);
+            })
+        } 
+        },
+    
+    created() {
+        this.getProjects();
+    },
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
